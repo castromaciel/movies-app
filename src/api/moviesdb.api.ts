13bd3cca76cb6ceab8a6bb1e52eaf0909c4d68f9
castromaciel <castromaciel00@gmail.com>
2023-12-03 18:14:32 -0300

    delete api key

--- src/api/moviesdb.api.ts
+++ src/api/moviesdb.api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const moviesDbApi = axios.create({
   baseURL: 'https://api.themoviedb.org/3/movie',
   params: {
-    api_key: 'a0a6c5bcc2aa33755c5c8780ad193763',
+    api_key: 'YOUR_API_KEY',
     language: 'en-EN'
   }
 })
