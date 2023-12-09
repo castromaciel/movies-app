741a947bf192be4cfe25e5f4f3de1a0bac1233ee
castromaciel <castromaciel00@gmail.com>
2023-12-09 15:25:25 -0300

    add gradient provider

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { FC } from 'react';
 import { StyleSheet } from 'react-native';
 import 'react-native-gesture-handler';
+import { GradientProvider } from './src/context';
 import Navigation from './src/navigation';
 
 interface AppProps {}
@@ -9,8 +10,10 @@ interface AppProps {}
 const App: FC<AppProps> = () => {
   return (
     <NavigationContainer>
-      <Navigation />
-    </NavigationContainer>    
+      <GradientProvider>
+        <Navigation />
+      </GradientProvider>
+    </NavigationContainer>
   )
 }
 
